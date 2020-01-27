fcb7c8d5c2ee385a3b24ffbfa2c52e94180c8bad
james94 <11840449+james94@users.noreply.github.com>
2020-01-27 11:06:42 -0800

    Added Python code for creating and printing array

---
+++ projects/Python/Essentials/Array/main.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__":
+    # Declare and Initialize Array
+    B = [2,4,6,8,10]
+
+    # Access and Print Array Elements
+    for x in B:
+        print("{}".format(x), end = " ")
