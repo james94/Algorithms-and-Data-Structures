395b6de4ac096fdc7360f6e81d81ce00116061e3
james94 <11840449+james94@users.noreply.github.com>
2020-01-30 07:41:28 -0800

    Added print program name and newline in code

--- projects/Python/Essentials/Array/main.py
+++ projects/Python/Essentials/Array/main.py
@@ -1,8 +1,10 @@
 
 if __name__ == "__main__":
+    print("*********** Array Basics Program ***********")
     # Declare and Initialize Array
     B = [2,4,6,8,10]
 
     # Access and Print Array Elements
     for x in B:
         print("{}".format(x), end = " ")
+    print()
