184e99dc45307e760fb4a79173e89de12fd61966
james94 <11840449+james94@users.noreply.github.com>
2020-02-06 07:05:46 -0800

    Added Python Functions program

---
+++ projects/Python/Essentials/Functions/main.py
@@ -0,0 +1,17 @@
+
+def add(a, b):
+    """Add two numbers"""
+    c = a+b
+    return c
+
+# Define main method that calls other functions
+def main():
+    print("*********** Functions Program ***********")
+    x = 10
+    y = 5
+    z = add(x, y)
+    print("sum is ", z)
+
+# Execute main() function
+if __name__ == "__main__":
+    main()
