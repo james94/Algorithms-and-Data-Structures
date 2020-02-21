744d93cee67217021c783aa6e36deb7806aef73e
james94 <11840449+james94@users.noreply.github.com>
2020-02-21 08:31:57 -0800

    Added a check if build/ dir exists

--- C/Essentials/Array/compile.sh
+++ C/Essentials/Array/compile.sh
@@ -4,6 +4,14 @@
 # Use CMake to automate building Makefile for project
 # Compile project using Makefile
 
+# Create build/ dir if does not exist
+if [ ! -d "build/" ]
+then
+    mkdir build
+else
+    echo "Directory build/ exists"
+fi
+
 # Place all of build files (Makefile, etc) in build dir
 cd build && cmake ..
 
