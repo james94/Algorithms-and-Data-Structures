c8ea12eb9392508ad42b63a6fdfd5993ef48dbcb
james94 <11840449+james94@users.noreply.github.com>
2020-03-13 06:15:18 -0700

    Added Change Case Of String functions

--- C/Strings/StringOpsLibShared/include/StringOperations.h
+++ C/Strings/StringOpsLibShared/include/StringOperations.h
@@ -4,4 +4,13 @@
 // Find Length of a String
 void FindLength(char *s);
 
+// Change case of String to Upper Case
+void ChangeCaseToUpper(char *s);
+
+// Change case of String to Lower Case
+void ChangeCaseToLower(char *s);
+
+// Toggle case of String
+void ToggleCase(char *s);
+
 #endif
--- C/Strings/StringOpsLibShared/src/StringOperations.c
+++ C/Strings/StringOpsLibShared/src/StringOperations.c
@@ -13,3 +13,46 @@ void FindLength(char *s)
     }
     printf("Length is %d\n", i);
 }
+
+// Change case of String to Upper Case
+// 65=A, 97=a; diff=32; Subtract to get Upper Case
+void ChangeCaseToUpper(char *s)
+{
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] -= 32;
+        }
+    }
+}
+
+// Change case of String to Lower Case
+// 65=A, 97=a; diff=32; Add to get Lower Case
+void ChangeCaseToLower(char *s)
+{
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        if(s[i] >= 'A' && s[i] <= 'Z')
+        {
+            s[i] += 32;
+        }
+    }
+}
+
+// Toggle case of String
+void ToggleCase(char *s)
+{
+    for(int i = 0; s[i] != '\0'; i++)
+    {
+        // 65=A && 90=Z is it upper case
+        if(s[i] >= 65 && s[i] <= 90)
+        {
+            s[i] += 32;
+        }// 97=a && 122=z is it lower case
+        else if(s[i] >= 97 && s[i] <= 122)
+        {
+            s[i] -= 32;
+        }
+    }
+}
