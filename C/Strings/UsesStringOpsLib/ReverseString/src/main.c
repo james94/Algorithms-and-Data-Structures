50ba8dcf99bd6b5e8354d829e7636896263a767e
james94 <11840449+james94@users.noreply.github.com>
2020-03-14 15:39:46 -0700

    Updated Reverse C-String C example
    Show how to use both Methods to reverse C-String.
    Show how to use Reverse1(char *s, char *r) and
    then how to use Reverse2(char *s) function.

--- C/Strings/UsesStringOpsLib/ReverseString/src/main.c
+++ C/Strings/UsesStringOpsLib/ReverseString/src/main.c
@@ -6,9 +6,15 @@ int main()
     // Check is string valid
     char A[] = "python";
     char B[7];
+    printf("Method 1: Reverse String\n");
     printf("Will Reverse String: %s\n", A);
-    Reverse(A, B);
-    printf("Reversed String:\n");
-    printf("%s", B);
+    Reverse1(A, B);
+    printf("Reversed String: %s\n", B);
+
+    char C[] = "javascript";
+    printf("\nMethod 2: Reverse String\n");
+    printf("Will Reverse String: %s\n", C);
+    Reverse2(C);
+    printf("Reversed String: %s\n", C);
     return 0;
 }
